9b16da4c59f2c5241f1c852d99d75c505e3091a5
yaohongdi <yaokevin1987@gmail.com>
2020-04-12 00:46:24 +0800

    2-11:v1

---
+++ chapter2/2-11.py
@@ -0,0 +1,11 @@
+#-*-coding:utf-8-*-
+
+while True:
+    put = raw_input("please enter you choice :")
+    if put == '1':
+        print("you will sum ")
+        print(sum([1,2,3,4,5]))
+    elif put == '2':
+        print("you will avg")
+    elif put == 'x':
+        break
