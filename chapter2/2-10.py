ee60fa58f7739dce87e4279c5d1033a52743a630
yaohongdi <yaokevin1987@gmail.com>
2020-04-11 23:52:32 +0800

    2-10:v2

--- chapter2/2-10.py
+++ chapter2/2-10.py
@@ -1,9 +1,12 @@
 #-*-coding:utf-8-*-
 
-input1 = int(raw_input("please input a number:"))
 
-if input1 >0 and input1 < 100:
-    print("success")
-    exit
-
+while True:
+  input1 = int(raw_input("please input a number:"))
+  if input1 >0 and input1 < 100:
+      print("success")
+      break
+  else :
+      print("please input again")
+      continue
 
